5e3a649b4ad7612483b7ebbb4ae1a152170f7597
chrystalio <chrystalio.k@outlook.com>
2022-12-04 01:48:07 +0700

    Add route to detail blade

--- resources/views/pages/_home.blade.php
+++ resources/views/pages/_home.blade.php
@@ -62,7 +62,7 @@
                             <div class="travel-country">INDONESIA</div>
                             <div class="travel-location">DERATAN, BALI</div>
                             <div class="travel-button mt-auto">
-                                <a href="/details" class="btn btn-travel-details px-4">
+                                <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
                                     View Details
                                 </a>
                             </div>
@@ -76,7 +76,7 @@
                             <div class="travel-country">INDONESIA</div>
                             <div class="travel-location">BROMO, MALANG</div>
                             <div class="travel-button mt-auto">
-                                <a href="/details" class="btn btn-travel-details px-4">
+                                <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
                                     View Details
                                 </a>
                             </div>
@@ -90,7 +90,7 @@
                             <div class="travel-country">INDONESIA</div>
                             <div class="travel-location">NUSA PENIDA</div>
                             <div class="travel-button mt-auto">
-                                <a href="/details" class="btn btn-travel-details px-4">
+                                <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
                                     View Details
                                 </a>
                             </div>
