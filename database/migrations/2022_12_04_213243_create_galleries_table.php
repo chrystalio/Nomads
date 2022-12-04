9568be29330bdcef22fab59c7c19c725e725eee5
chrystalio <chrystalio.k@outlook.com>
2022-12-04 21:35:09 +0700

    Add galleries migration and implement softdeletes

--- database/migrations/2022_12_04_213243_create_galleries_table.php
+++ database/migrations/2022_12_04_213243_create_galleries_table.php
@@ -9,9 +9,9 @@ return new class extends Migration {
     {
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
-
-
-
+            $table->foreignId('travel_packages_id')->constrained('travel_packages');
+            $table->text('image');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
