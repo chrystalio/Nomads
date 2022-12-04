f0735c46fbd2062529c8a3ca783f930808c535c5
chrystalio <chrystalio.k@outlook.com>
2022-12-04 21:03:20 +0700

    Refactor CheckoutController routes, set it to be route controller

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/detail', [DetailController::class, 'index'])->name('detail');
-Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
-Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
+
+Route::controller(CheckoutController::class)->group(function () {
+    Route::get('/checkout', 'index')->name('checkout');
+    Route::get('/checkout/status', 'process')->name('checkout-success');
+});
 
 Route::prefix('admin')->namespace('Admin')->group(function(){
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
