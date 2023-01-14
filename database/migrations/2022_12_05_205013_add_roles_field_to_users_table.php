23898c17167cc691a549414ca4e8edc8c9ec3a3c
chrystalio <chrystalio.k@outlook.com>
2023-01-14 22:06:55 +0700

    Update migration roles

--- database/migrations/2022_12_05_205013_add_roles_field_to_users_table.php
+++ database/migrations/2022_12_05_205013_add_roles_field_to_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('roles')->default('USER'); // USER, ADMIN
+            $table->string('roles')->after('password')->default('User'); // USER, ADMIN
         });
     }
 
