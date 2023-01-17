0b363b2a1cb405233801522cc3a23fb927acdc0f
chrystalio <chrystalio.k@outlook.com>
2023-01-17 22:09:29 +0700

    Add form travel packages

---
+++ resources/views/pages/admin/travel-package/_create.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts._admin')
+@section('title', 'Nomads Dashboard')
+@section('content')
+    <div class="page-heading d-flex justify-content-between">
+        <h3>Nomads Dashboard</h3>
+    </div>
+    <div class="page-content vh-100">
+        <h4></h4>
+        <div class="row my-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <section id="multiple-column-form">
+                            <div class="row match-height">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h4 class="card-title">Add Travel Packages</h4>
+                                        </div>
+                                        <div class="card-content">
+                                            <div class="card-body">
+                                                <form action="{{route('travel-package.store')}}" method="post" enctype="multipart/form-data">
+                                                    @csrf
+                                                    <div class="row">
+                                                        <div class="col-md-6 col-12">
+                                                            <div class="form-group">
+                                                                <label for="title">Title</label>
+                                                                <input type="text" id="title" class="form-control" placeholder="Title" name="title">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6 col-12">
+                                                            <div class="form-group">
+                                                                <label for="location">Location</label>
+                                                                <input type="text" id="location" class="form-control" placeholder="Location" name="location">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6 col-12">
+                                                            <div class="form-group">
+                                                                <label for="type">Type</label>
+                                                                <input type="text" id="type" class="form-control" placeholder="Type" name="type">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6 col-12">
+                                                            <div class="form-group">
+                                                                <label for="departure_date">Departure Date</label>
+                                                                <input type="date" id="departure_date" class="form-control" placeholder="Departure Date" name="departure_date">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6 col-12">
+                                                            <div class="form-group">
+                                                                <label for="duration">Duration</label>
+                                                                <input type="text" id="duration" class="form-control" placeholder="Duration" name="duration">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6 col-12">
+                                                            <div class="form-group">
+                                                                <label for="price">Price</label>
+                                                                <input type="text" id="price" class="form-control" placeholder="Price" name="price">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12 my-2 d-flex justify-content-end">
+                                                            <button type="submit" class="btn btn-primary me-1 mb-1">
+                                                                Submit
+                                                            </button>
+                                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                                                                Reset
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
