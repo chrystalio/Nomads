6bdd35279e08454d44d07c93679178d73fe89cf4
chrystalio <chrystalio.k@outlook.com>
2023-01-18 22:32:53 +0700

    Setup request validation

--- app/Http/Requests/Admin/GalleryRequest.php
+++ app/Http/Requests/Admin/GalleryRequest.php
@@ -9,16 +9,8 @@ class GalleryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:255',
-            'location' => 'required|string|max:255',
-            'about' => 'required|string',
-            'featured_event' => 'required|string|max:255',
-            'language' => 'required|string|max:255',
-            'foods' => 'required|string|max:255',
-            'departure_date' => 'required|date',
-            'duration' => 'required|string|max:255',
-            'type' => 'required|string|max:255',
-            'price' => 'required|integer',
+            'travel_packages_id' => 'required|integer|exists:travel_packages,id',
+            'image' => 'required|image',
         ];
     }
 
