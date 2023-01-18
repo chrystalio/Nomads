e1a5f6a2b4f85181bf4f0886240bbe11c064fa5e
chrystalio <chrystalio.k@outlook.com>
2023-01-18 22:32:37 +0700

    Add request validation for gallery

---
+++ app/Http/Requests/Admin/GalleryRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GalleryRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'location' => 'required|string|max:255',
+            'about' => 'required|string',
+            'featured_event' => 'required|string|max:255',
+            'language' => 'required|string|max:255',
+            'foods' => 'required|string|max:255',
+            'departure_date' => 'required|date',
+            'duration' => 'required|string|max:255',
+            'type' => 'required|string|max:255',
+            'price' => 'required|integer',
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
