1c6444befd5ff9deece54dcb0f7605dbb4b91670
chrystalio <chrystalio.k@outlook.com>
2023-01-18 22:24:39 +0700

    Setup model fillable and relational

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -4,6 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Galery extends Model
+class Gallery extends Model
 {
+    protected $fillable = [
+        'travel_packages_id',
+        'image',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function travel_package()
+    {
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id', 'id');
+    }
 }
