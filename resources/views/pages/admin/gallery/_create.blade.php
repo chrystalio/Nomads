e2afa2539ed7911bf8d7c8a6e9ddf90a7bce2bb3
chrystalio <chrystalio.k@outlook.com>
2023-01-19 21:51:32 +0700

    Add create blade for gallery

---
+++ resources/views/pages/admin/gallery/_create.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts._admin')
+@section('title', 'Nomads Dashboard')
+@section('content')
+    <div class="page-heading d-flex justify-content-between">
+        <h3>Nomads Dashboard</h3>
+    </div>
+    <div class="page-content">
+        <h4></h4>
+        <div class="row my-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <section id="multiple-column-form">
+                            <div class="row match-height">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h4 class="card-title">Add Image</h4>
+                                        </div>
+                                        <div class="card-content">
+                                            <div class="card-body">
+                                                @if($errors->any())
+                                                    <div class="alert alert-danger">
+                                                        <ul>
+                                                            @foreach($errors->all() as $error)
+                                                                <li>{{$error}}</li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
+                                                @endif
+                                                <form action="{{route('gallery.store')}}" method="post" enctype="multipart/form-data">
+                                                    @csrf
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="form-group">
+                                                                <label for="travel_packages_id">Travel Package</label>
+                                                                <select name="travel_packages_id" id="travel_packages_id" class="form-control">
+                                                                    <option value="">Select Travel Package</option>
+                                                                    @foreach($travel_packages as $travel_package)
+                                                                        <option value="{{$travel_package->id}}">{{$travel_package->title}}</option>
+                                                                    @endforeach
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="form-group">
+                                                                <label for="image">Image</label>
+                                                                <input type="file" id="image" class="form-control" name="image">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12 my-2 d-flex justify-content-end">
+                                                            <button type="submit" class="btn btn-primary me-3 mb-1">
+                                                                Submit
+                                                            </button>
+                                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                                                                Reset
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
