698c7fd70c89b9f6db3ebab55bfdc40aed135edd
chrystalio <chrystalio.k@outlook.com>
2023-01-20 11:06:33 +0700

    feature/transactions Add Transaction Controller

---
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(transaction $transactions)
+    {
+    }
+
+    public function edit(transaction $transactions)
+    {
+    }
+
+    public function update(Request $request, transaction $transactions)
+    {
+    }
+
+    public function destroy(transaction $transactions)
+    {
+    }
+}
