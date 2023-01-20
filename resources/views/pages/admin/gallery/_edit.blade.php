2ebc24e8885fe4d47ca3802fe971ce83907bc393
chrystalio <chrystalio.k@outlook.com>
2023-01-20 10:25:49 +0700

    Set edit function for galery

---
+++ resources/views/pages/admin/gallery/_edit.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts._admin')
+@section('title', 'Nomads Dashboard')
+@section('content')
+    <div class="page-heading d-flex justify-content-between">
+        <h3>Nomads Dashboard</h3>
+    </div>
+    <div class="page-content">
+        <h4></h4>
+        <div class="row my-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <section id="multiple-column-form">
+                            <div class="row match-height">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h4 class="card-title">Edit Image - {{$item->travel_package->title}}</h4>
+                                        </div>
+                                        <div class="card-content">
+                                            <div class="card-body">
+                                                @if($errors->any())
+                                                    <div class="alert alert-danger">
+                                                        <ul>
+                                                            @foreach($errors->all() as $error)
+                                                                <li>{{$error}}</li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
+                                                @endif
+                                                <form action="{{route('gallery.update', $item->id )}}" method="post" enctype="multipart/form-data">
+                                                    @method('PUT')
+                                                    @csrf
+                                                    <div class="row">
+                                                        <div class="col-12">
+                                                            <div class="form-group">
+                                                                <label for="travel_packages_id">Travel Package</label>
+                                                                <select name="travel_packages_id" id="travel_packages_id" class="form-control">
+                                                                    <option value="{{$item->travel_packages_id}}">{{$item->travel_package->title}}</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="form-group">
+                                                                <label for="image">Image</label>
+                                                                    <input type="file" id="image" class="form-control" name="image">
+                                                                    <small>
+                                                                        <i>
+                                                                            <span class="text-danger">
+                                                                                *Leave it blank if you don't want to change the image
+                                                                            </span>
+                                                                        </i>
+                                                                    </small>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12 my-2 d-flex justify-content-end">
+                                                            <button type="submit" class="btn btn-primary me-3 mb-1">
+                                                                Submit
+                                                            </button>
+                                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                                                                Reset
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
