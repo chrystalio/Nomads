2cee22e8202e7f0760cc5f3afb72316755a8427b
chrystalio <chrystalio.k@outlook.com>
2023-01-22 00:23:33 +0700

    feature/transactions Set method edit and update on TransactionController

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Alert;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
@@ -34,12 +36,34 @@ class TransactionController extends Controller
         ]);
     }
 
-    public function edit(transaction $transactions)
+    public function edit($id)
     {
+        $item = Transaction::with(['details', 'travel_package', 'user'])->findOrFail($id);
+
+        return view('pages.admin.transaction._edit',[
+            'item' => $item,
+        ]);
     }
 
-    public function update(Request $request, transaction $transactions)
+    public function update(Request $request, $id): \Illuminate\Http\RedirectResponse
     {
+        $data = $request->all();
+
+        $item = Transaction::findOrFail($id);
+
+        DB::beginTransaction();
+        try {
+            $item->update($data);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->withErrors($e->getMessage())->withInput($request->all());
+        }
+        DB::commit();
+
+        // Add alert message here using Prologue Alerts
+        Alert::success('Status has been updated successfully')->flash();
+
+        return redirect()->route('transaction.index');
     }
 
     public function destroy(transaction $transactions)
