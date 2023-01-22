6167dc9e6b2cb9951a57964543adea4240b14b39
chrystalio <chrystalio.k@outlook.com>
2023-01-22 17:22:22 +0700

    integration/fe foreach data to card destination

--- resources/views/pages/_home.blade.php
+++ resources/views/pages/_home.blade.php
@@ -12,7 +12,7 @@
             <br/>
             moment you never see before
         </p>
-        <a href="#" class="btn btn-get-started px-4 mt-4">
+        <a href="#popular" class="btn btn-get-started px-4 mt-4">
             Get Started
         </a>
     </header>
@@ -54,62 +54,23 @@
         <section class="section-popular-content" id="popularContent">
             <div class="container">
                 <div class="section-popular-travel row justify-content-center">
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div
-                            class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/travel-1@2x.jpg');"
-                        >
-                            <div class="travel-country">INDONESIA</div>
-                            <div class="travel-location">DERATAN, BALI</div>
-                            <div class="travel-button mt-auto">
-                                <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
+                    @foreach($items as $item)
+                        <div class="col-sm-6 col-md-4 col-lg-3 ">
+                            <div
+                                class="card-travel text-center d-flex flex-column rounded-lg shadow-md"
+                                style="background-image: url('{{$item->galleries->count() ?  Storage::url($item->galleries->first()->image) : 'no image'}}');">
+                                <div class="travel-country text-uppercase">{{$item->location}}</div>
+                                <div class="travel-location">
+                                    {{$item->title}}
+                                </div>
+                                <div class="travel-button mt-auto">
+                                    <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
+                                        View Details
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div
-                            class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/travel-2@2x.jpg');"
-                        >
-                            <div class="travel-country">INDONESIA</div>
-                            <div class="travel-location">BROMO, MALANG</div>
-                            <div class="travel-button mt-auto">
-                                <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div
-                            class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/travel-3@2x.jpg');"
-                        >
-                            <div class="travel-country">INDONESIA</div>
-                            <div class="travel-location">NUSA PENIDA</div>
-                            <div class="travel-button mt-auto">
-                                <a href="{{route('detail')}}" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div
-                            class="card-travel text-center d-flex flex-column"
-                            style="background-image: url('frontend/images/travel-4@2x.jpg')"
-                        >
-                            <div class="travel-country">UAE</div>
-                            <div class="travel-location">DUBAI</div>
-                            <div class="travel-button mt-auto">
-                                <a href="details.html" class="btn btn-travel-details px-4">
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </section>
