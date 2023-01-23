e5df2828ab2ccaeda30723f9a9ac607542372703
chrystalio <chrystalio.k@outlook.com>
2023-01-23 20:12:46 +0700

    integration/fe Set route on checkout process on form action

--- resources/views/pages/_detail.blade.php
+++ resources/views/pages/_detail.blade.php
@@ -116,7 +116,7 @@
                             @endforeach
                             <div class="join-container">
                                 @auth()
-                                    <form action="" method="POST">
+                                    <form action="{{ route('checkout-process', $item->id) }}" method="POST">
                                         @csrf
                                         <button class="btn btn-block btn-join-now mt-3 py-2" type="submit">
                                             Join Now
