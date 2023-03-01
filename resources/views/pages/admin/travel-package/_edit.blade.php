e3ce7143cc2d46bb562d4e1c8f909499d3d7fc2e
chrystalio <chrystalio.k@outlook.com>
2023-03-01 12:41:04 +0700

    Set select option for type field edit

--- resources/views/pages/admin/travel-package/_edit.blade.php
+++ resources/views/pages/admin/travel-package/_edit.blade.php
@@ -28,75 +28,105 @@
                                                         </ul>
                                                     </div>
                                                 @endif
-                                                <form action="{{route('travel-package.update', $item->id)}}" method="post" enctype="multipart/form-data">
+                                                <form action="{{route('travel-package.update', $item->id)}}"
+                                                      method="post" enctype="multipart/form-data">
                                                     @method('PUT')
                                                     @csrf
                                                     <div class="row">
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="title">Title</label>
-                                                                <input type="text" id="title" class="form-control" placeholder="Title" name="title" value="{{$item->title}}">
+                                                                <input type="text" id="title" class="form-control"
+                                                                       placeholder="Title" name="title"
+                                                                       value="{{$item->title}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="location">Location</label>
-                                                                <input type="text" id="location" class="form-control" placeholder="Location" name="location" value="{{$item->location}}">
+                                                                <input type="text" id="location" class="form-control"
+                                                                       placeholder="Location" name="location"
+                                                                       value="{{$item->location}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-12">
                                                             <div class="form-group">
                                                                 <label for="about">About</label>
-                                                                <textarea name="about" id="about" rows="3" class="form-control">{{$item->about}}</textarea>
+                                                                <textarea name="about" id="about" rows="3"
+                                                                          class="form-control">{{$item->about}}</textarea>
                                                             </div>
                                                         </div>
                                                         <div class="col-12">
                                                             <div class="form-group">
                                                                 <label for="departure_date">Departure Date</label>
-                                                                <input type="date" id="departure_date" class="form-control" placeholder="Departure Date" name="departure_date" value="{{$item->departure_date}}">
+                                                                <input type="date" id="departure_date"
+                                                                       class="form-control" placeholder="Departure Date"
+                                                                       name="departure_date"
+                                                                       value="{{$item->departure_date}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="type">Type</label>
-                                                                <input type="text" id="type" class="form-control" placeholder="Type" name="type" value="{{$item->type}}">
+                                                                <select name="type" id="type" class="form-control">
+                                                                    <option
+                                                                        value="{{$item->type}}">{{$item->type}}</option>
+                                                                    @if($item->type === 'Open Trip')
+                                                                        <option value="Private Trip">Private Trip
+                                                                        </option>
+                                                                    @elseif($item->type === 'Private Trip')
+                                                                        <option value="Open Trip">Open Trip</option>
+                                                                    @endif
+                                                                </select>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="language">Language</label>
-                                                                <input type="text" id="language" class="form-control" placeholder="Language" name="language" value="{{$item->language}}">
+                                                                <input type="text" id="language" class="form-control"
+                                                                       placeholder="Language" name="language"
+                                                                       value="{{$item->language}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="foods">Foods</label>
-                                                                <input type="text" id="foods" class="form-control" placeholder="Foods" name="foods" value="{{$item->foods}}">
+                                                                <input type="text" id="foods" class="form-control"
+                                                                       placeholder="Foods" name="foods"
+                                                                       value="{{$item->foods}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="duration">Duration</label>
-                                                                <input type="text" id="duration" class="form-control" placeholder="Duration" name="duration" value="{{$item->duration}}">
+                                                                <input type="text" id="duration" class="form-control"
+                                                                       placeholder="Duration" name="duration"
+                                                                       value="{{$item->duration}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="featured_event">Featured Events</label>
-                                                                <input type="text" id="featured_event" class="form-control" placeholder="Featured Event" name="featured_event" value="{{$item->featured_event}}">
+                                                                <input type="text" id="featured_event"
+                                                                       class="form-control" placeholder="Featured Event"
+                                                                       name="featured_event"
+                                                                       value="{{$item->featured_event}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-md-6 col-12">
                                                             <div class="form-group">
                                                                 <label for="price">Price</label>
-                                                                <input type="number" id="price" class="form-control" placeholder="Rp." name="price" value="{{$item->price}}">
+                                                                <input type="number" id="price" class="form-control"
+                                                                       placeholder="Rp." name="price"
+                                                                       value="{{$item->price}}">
                                                             </div>
                                                         </div>
                                                         <div class="col-12 my-2 d-flex justify-content-end">
                                                             <button type="submit" class="btn btn-primary me-3 mb-1">
                                                                 Edit
                                                             </button>
-                                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                                                            <button type="reset"
+                                                                    class="btn btn-light-secondary me-1 mb-1">
                                                                 Reset
                                                             </button>
                                                         </div>
