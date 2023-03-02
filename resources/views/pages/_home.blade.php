57dd8ce6e4aee87627a3e1cb471a9c013b2ef90e
chrystalio <chrystalio.k@outlook.com>
2023-03-02 12:41:04 +0700

    If travel packages has no image then hide

--- resources/views/pages/_home.blade.php
+++ resources/views/pages/_home.blade.php
@@ -55,21 +55,20 @@
             <div class="container">
                 <div class="section-popular-travel row justify-content-center">
                     @foreach($items as $item)
-                        <div class="col-sm-6 col-md-4 col-lg-3 ">
-                            <div
-                                class="card-travel text-center d-flex flex-column rounded-lg shadow-md"
-                                style="background-image: url('{{$item->galleries->count() ?  Storage::url($item->galleries->first()->image) : 'no image'}}');">
-                                <div class="travel-country text-uppercase">{{$item->location}}</div>
-                                <div class="travel-location">
-                                    {{$item->title}}
-                                </div>
-                                <div class="travel-button mt-auto">
-                                    <a href="{{route('detail', $item->slug)}}" class="btn btn-travel-details px-4">
-                                        View Details
-                                    </a>
+                        @if($item->galleries->count())
+                            <div class="col-sm-6 col-md-4 col-lg-3">
+                                <div class="card-travel text-center d-flex flex-column"
+                                     style="background-image: url('{{Storage::url($item->galleries->first()->image)}}');">
+                                    <div class="travel-country">{{$item->location}}</div>
+                                    <div class="travel-location">{{$item->title}}</div>
+                                    <div class="travel-button mt-auto">
+                                        <a href="{{route('detail', $item->slug)}}" class="btn btn-travel-details px-4">
+                                            View Details
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        @endif
                     @endforeach
                 </div>
             </div>
