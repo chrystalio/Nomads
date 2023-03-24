ff6c0512334194722b8eb41911f01623db1257e8
chrystalio <chrystalio.k@outlook.com>
2023-03-24 11:31:24 +0700

    Add mail blade (unfinished might be redesigned in the future

---
+++ resources/views/emails/transaction_success.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Transaction Success</title>
+</head>
+<body>
+<p>Dear {{$user->name}},</p>
+
+<p>We are happy to inform you that your payment for {{$travel_package->title}} has been successfully processed.</p>
+
+<p>Your e-ticket is attached to this email. Please print it out or save it on your mobile device and present it at the check-in counter.</p>
+
+<p>Thank you for choosing our travel service. We hope you enjoy your trip!</p>
+
+<p>Best regards,</p>
+
+<p>Nomads</p>
+</body>
+</html>
