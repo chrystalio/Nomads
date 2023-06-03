54678d4868e623af7310bdedf5674eda381929c8
Chrystalio <chrystalio.k@outlook.com>
2023-06-03 13:38:13 +0700

    Update gallery model, implement uuid as primary key

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Gallery extends Model
 {
+    use HasUuids;
+
+    protected $primaryKey = 'uuid';
+
     protected $fillable = [
+        'uuid',
         'travel_packages_uuid',
         'image',
     ];
